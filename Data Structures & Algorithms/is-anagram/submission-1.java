919adb3465500e0c77501d3fbd15f235b1f36845
VikramSitaram <vik_sit@yahoo.co.in>
2026-07-24 15:20:19 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null) {
+            return false;
+        }
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        if (s.equals(t)) {
+            return true;
+        }
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            if (sMap.containsKey(currChar)) {
+                sMap.put(currChar, sMap.get(currChar)+1);
+            } else {
+                sMap.put(currChar, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char currChar = t.charAt(i);
+            if (tMap.containsKey(currChar)) {
+                tMap.put(currChar, tMap.get(currChar)+1);
+            } else {
+                tMap.put(currChar, 1);
+            }
+        }
+
+        return sMap.equals(tMap);
+    }
+}
