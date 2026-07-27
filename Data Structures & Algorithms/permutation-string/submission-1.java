9d95c9d0ca67bc52cd9ac554860dc8986ac18b0c
VikramSitaram <vik_sit@yahoo.co.in>
2026-07-26 20:41:26 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s2.length() < s2.length()) {
+            return false;
+        }
+
+        int[] s1Arr = new int[26];
+        for (char c : s1.toCharArray()) {
+            s1Arr[c-'a']++;
+        }
+        String s1ASCII = Arrays.toString(s1Arr);
+
+        int l = 0, r = s1.length();
+        while (r <= s2.length()) {
+            String currSubstr = s2.substring(l++, r++);
+            int[] s2SubArr = new int[26];
+            for (char c : currSubstr.toCharArray()) {
+                s2SubArr[c-'a']++;
+            }
+            String s2SubASCII = Arrays.toString(s2SubArr);
+            if (s2SubASCII.equals(s1ASCII)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
