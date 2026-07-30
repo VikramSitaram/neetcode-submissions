0aebe8f808bf9cf56b3268b2bd6be13cca433702
VikramSitaram <vik_sit@yahoo.co.in>
2026-07-29 22:20:37 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+
+        int max = 0;
+        int i = 1;
+        int j = 0;
+
+        while (i <= prices.length - 1) {
+            if (prices[i] > prices[j]) {
+                int diff = prices[i] - prices[j];
+                max = (diff > max) ? diff : max;
+                i++;
+            } else {
+                j = i;
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
