29dee38d964acf4fac3e96990d8fa172421cf616
VikramSitaram <vik_sit@yahoo.co.in>
2026-08-01 13:47:29 -0700

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int dfs(int[][] grid, int r, int c, Set visited) {
+        // base checks
+        if(r < 0 || r >= grid.length
+            || c < 0 || c >= grid[0].length
+            || visited.contains(r + "#" + c)
+            || grid[r][c] == 0) {
+                return 0;
+            }
+
+            // mark cell as visited
+            visited.add(r + "#" + c);
+
+            // recurse and return sum (add +1 for itself)
+            return dfs(grid, r - 1, c, visited) + dfs(grid, r + 1, c, visited) + dfs(grid, r, c - 1, visited) + dfs(grid, r, c + 1, visited) + 1;
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        Set<String> visited = new HashSet<>();
+        int maxArea = 0;
+
+        // look at each cell
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                // check if island
+                if (grid[r][c] == 1 && !visited.contains(r + "#" + c)) {
+                    // run dfs starting from this cell
+                    int area = dfs(grid, r, c, visited);
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
