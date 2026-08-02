5c5125169a4678788469a05d6e77ac0b32fef256
VikramSitaram <vik_sit@yahoo.co.in>
2026-08-01 20:32:07 -0700

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // optimize lookup times to O(1)
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        int n = s.length();
+        boolean[] memo = new boolean[n + 1];
+
+        // base case: empty prefix is always valid
+        memo[0] = true;
+
+        // loop through all prefix lengths
+        for (int i = 0; i <= n; i++) {
+            // check for valid cutting points
+            for (int j = 0; j < i; j++) {
+                if (memo[j] && wordSet.contains(s.substring(j, i))) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return memo[n];
+    }
+}
