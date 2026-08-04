8da03f2de7f93d5003b266d1ad7e2bcad223b2cc
VikramSitaram <vik_sit@yahoo.co.in>
2026-08-03 20:34:13 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> closingBrackets = new ArrayDeque<>();
+
+        if (s.length() %2 != 0) {
+            return false;
+        }
+
+        for (int i = 0;i < s.length(); i++) {
+            char bracket = s.charAt(i);
+            if (bracket == '{') {
+                closingBrackets.push('}');
+            } else if (bracket == '[') {
+                closingBrackets.push(']');
+            } else if (bracket == '(') {
+                closingBrackets.push(')');
+            } else {
+                if (closingBrackets.isEmpty()) {
+                    return false;
+                } else if (closingBrackets.pop() != bracket) {
+                    return false;
+                }
+            }
+        }
+
+        return closingBrackets.isEmpty();
+    }
+}
